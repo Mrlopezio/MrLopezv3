53ac5dcd7cc377175eb1e71a68e779ce8162e2f3
Jabir Lopez <jlopez@itzamedia.com>
2024-10-08 13:56:41 +0100

    vercel analytics added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  integrations: [react()]
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	}),
+	integrations: [react()]
 })
