243092263dbb6353a963c7d65e9c8f199fc83d4d
Jabir Lopez <jlopez@itzamedia.com>
2024-10-08 13:44:12 +0100

    latest changes

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -8,11 +8,15 @@ type Props = {
 	logo: string
 	image: string
 	id: string
-	href: string
+	links: links[]
+}
+type links = {
+	text: string
+	url: string
 }
 
 const ProjectCard = (props: Props) => {
-	const { title, text, tags, logo, image, id, href } = props
+	const { title, text, tags, logo, image, id, links } = props
 	const { ref, inView } = useInView({
 		threshold: 0.7
 		// triggerOnce: true
@@ -88,6 +92,11 @@ const ProjectCard = (props: Props) => {
 					<p>{text}</p>
 				</div>
 				<div className="projects-content-slider-item-info-links">
+					{links.map((link: links, i: number) => (
+						<React.Fragment key={i}>
+							<Link text={link.text} url={link.url} />
+						</React.Fragment>
+					))}
 					{/*                     
 					<Link text="Latest project" href="#" />
 					<Link text="Live site" href="#" /> */}
@@ -98,3 +107,34 @@ const ProjectCard = (props: Props) => {
 }
 
 export default ProjectCard
+
+const Link = (props: links) => {
+	const { text, url } = props
+	return (
+		<a
+			href={url}
+			className="button-link"
+			target="_blank"
+			rel="nofollow noopener noreferrer external"
+		>
+			<div className="button-link-text">{text}</div>
+			<div className="button-link-arrow">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					className="lucide lucide-move-up-right"
+				>
+					<path d="M13 5H19V11" />
+					<path d="M19 5L5 19" />
+				</svg>
+			</div>
+		</a>
+	)
+}
