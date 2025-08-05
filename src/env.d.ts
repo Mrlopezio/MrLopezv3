b61b3e208865c02da826dffa4b85172798f0f45c
MrLopezio <mrlopezuk@gmail.com>
2025-08-05 08:14:40 +0100

    Add Google Analytics and update Lenis smooth scrolling integration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+
+// Google Analytics type declarations
+declare global {
+	interface Window {
+		dataLayer: any[]
+		gtag: (...args: any[]) => void
+	}
+
+	function gtag(...args: any[]): void
+	const dataLayer: any[]
+}
